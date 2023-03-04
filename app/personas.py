020d9654f59c1627e81338086d9520b9444b6432
Arne <arnchen@gmx.net>
2023-03-04 17:09:57 +0100

    added persona woman40

--- app/personas.py
+++ app/personas.py
@@ -25,3 +25,16 @@ transgender_women20 = [
     "In my ideal future, I am living a happy and fulfilling life surrounded by people I love and doing work that I find meaningful and impactful. I am healthy, financially secure, and contributing to the betterment of society.",
     "A future to avoid would be one where I am stuck in a job or lifestyle that makes me unhappy and unfulfilled. I want to avoid a future where I am not living up to my full potential or making a positive impact on the world."
 ]
+
+woman40 = [
+    "I would like to take more risks. As I get older, I find myself becoming more cautious and hesitant to try new things. But I know that growth and progress often come from stepping outside of our comfort zones. So I am working on cultivating a mindset of courage and openness to new experiences. I am reminding myself that failure is not the end of the world, and that taking a chance can lead to great rewards."
+    "I want to learn more about sustainable design practices and how I can integrate them into my work. By being mindful of my environmental impact, I can create designs that are both beautiful and responsible.",
+    "I want to improve my habit of procrastination by setting more concrete goals and deadlines for myself. By breaking down large tasks into smaller, more manageable steps, I can make steady progress towards my goals and avoid getting overwhelmed.",
+    "In the future, I want to maintain close relationships with my family and friends while also expanding my social circle through networking events and professional organizations. I believe that having a strong support network is essential for personal and professional success.",
+    "In my leisure time, I want to take up hiking and explore more of the natural beauty around me. I find that being in nature helps me to recharge and gain perspective, and I want to make this a regular part of my self-care routine.",
+    "In the future, I see myself as a proud grandparent, surrounded by a loving and supportive family. Family is important to me, and I want to continue to nurture these relationships as I age.",
+    "In the future, I see myself as a respected and influential figure in the design world, with a thriving business and a team of talented collaborators. I want to continue to push the boundaries of what is possible in design and inspire others to do the same.",
+    "I admire people who are resilient, compassionate, and self-motivated. These qualities inspire me to be a better person and strive towards my goals, even in the face of adversity.",
+    "In my ideal future, I am still working as a graphic designer, but I have expanded my business to include more clients and larger projects. I have a strong network of colleagues and collaborators who inspire me and challenge me to grow. I am healthy, active, and surrounded by loved ones who support and uplift me. And I am making a positive impact on the world through my work and my relationships.",
+    "A future that I want to avoid is one where I am stagnant, complacent, and disengaged from the world around me. I want to continue to learn, grow, and contribute in meaningful ways for as long as I am able. I want to live a life that is full of purpose and meaning, and that makes a positive difference in the world."
+]
