f9d49c04f9d7089d951e659bd991c169f0d63eb8
k-stz@thalassa <keistzen@gmail.com>
2023-03-04 20:53:36 +0100

    update personas with results

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import SurveyForm
-from app.personas import woman30
+from app.personas import woman30, transgender_woman20
 import os
 from app.classification import tags_from_answers
 
@@ -32,6 +32,26 @@ def persona():
     form = SurveyForm()
     form.question1.data = "blub"
     for field, answer in zip(form,woman30):
+        if field.type == "StringField":
+            field.data = answer
+            
+    if form.validate_on_submit():
+        answers = []
+        print(f"submited!")
+        for field in form:
+            if field.type == "StringField":
+                print(f"{field.name}={field.data}")
+                answers.append(field.data)
+        result = tags_from_answers(answers, tags)
+        print(result)
+    print(form.errors)
+    return render_template('index.html', form=form)
+
+@app.route('/persona2', methods=['GET', 'POST'])
+def persona2():
+    form = SurveyForm()
+    form.question1.data = "blub"
+    for field, answer in zip(form,transgender_woman20):
         if field.type == "StringField":
             field.data = answer
             
